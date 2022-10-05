a9d45d4122514037d6262d1d4f612db94900ece0
Astha <tripathiastha369@gmail.comgit config --global user.email tripathiastha369@gmail.comgit config --global user.email tripathiastha369@gmail.com>
2022-10-05 17:16:21 +0530

    05-10

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chat_view);
+        setContentView(R.layout.bluteooth1);
     }
 }
